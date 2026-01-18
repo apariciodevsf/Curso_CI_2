e5a0a1481f5cf79b9e9d11d4a930f780acd85000
Aparicio Junior <aparicio@cbxpert.com.br>
2026-01-18 01:09:10 -0300

    feat: configurar ambiente de desenvolvimento e CI/CD
    - Adicionar suporte a variáveis de ambiente (.env) com godotenv
    - Criar .gitignore para proteger arquivos sensíveis
    - Corrigir variável PORT para DB_PORT (conflito com Gin)
    - Atualizar PostgreSQL para versão 16 no docker-compose
    - Adicionar healthcheck ao PostgreSQL
    - Corrigir volume do PostgreSQL (named volume)
    - Atualizar workflow GitHub Actions:
      * Corrigir versões do Go (1.18, 1.19, 1.20)
      * Atualizar Ubuntu (22.04)
      * Adicionar espera pelo PostgreSQL estar healthy
      * Adicionar setup do Go no job build
    - Adicionar coleção Postman para testes da API
    - Criar .env.example como template

--- database/db.go
+++ database/db.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,8 +15,12 @@ var (
 )
 
 func ConectaComBancoDeDados() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Aviso: Arquivo .env não encontrado, usando variáveis de ambiente do sistema")
+	}
 	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DB_PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
